ac111544bbefcd24fbd57bd62a43321f596f9410
Git Automerge <jcguarinpenaranda@gmail.com>
2017-12-17 14:08:29 -0500

    exported more modules

--- test/index.test.jsx
+++ test/index.test.jsx
@@ -1,7 +1,16 @@
 import LoginModal2 from '../src/LoginModal';
 import ButtonInteract2 from '../src/ButtonInteract';
+import ButtonLike2 from '../src/ButtonLike';
+import ButtonFollow2 from '../src/ButtonFollow';
+import ButtonLogout2 from '../src/ButtonLogout';
 
-import { ButtonInteract, LoginModal } from '../src';
+import {
+  ButtonInteract,
+  LoginModal,
+  ButtonLike,
+  ButtonFollow,
+  ButtonLogout,
+} from '../src';
 
 test('should export ButtonInteract', () => {
   expect(ButtonInteract).toEqual(ButtonInteract2);
@@ -9,4 +18,16 @@ test('should export ButtonInteract', () => {
 
 test('should export LoginModal', () => {
   expect(LoginModal).toEqual(LoginModal2);
+});
+
+test('should export ButtonLike', () => {
+  expect(ButtonLike).toEqual(ButtonLike2);
+});
+
+test('should export ButtonFollow', () => {
+  expect(ButtonFollow).toEqual(ButtonFollow2);
+});
+
+test('should export ButtonLogout', () => {
+  expect(ButtonLogout).toEqual(ButtonLogout2);
 });
