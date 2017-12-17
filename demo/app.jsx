a26cb75dbd97c750aedaa3d05824fa6d968d4b54
Git Automerge <jcguarinpenaranda@gmail.com>
2017-12-17 11:11:49 -0500

    app now works

--- demo/app.jsx
+++ demo/app.jsx
@@ -1,5 +1,56 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Index from '../src/index';
+import 'babel-polyfill';
 
-render(<Index />, document.getElementById('root'));
+import { DataBrowser as Data } from '@owsas/geopromos-private-api/out/DataBrowser';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    // iniciar sesión para que el componente
+    // y todo lo demás que vayamos a mostrar
+    // pueda acceder a los datos privados del
+    // usuario con el que iniciarmemos sesión
+
+    this.logIn();
+    this.loadUserFeed();
+  }
+
+  async logIn() {
+    const user = await Data.getCurrentUser();
+
+    // Al iniciar sesión, se escribe en la consola la información del usuario
+    console.log('logged in as ', user && user.toJSON());
+
+    if (!user) {
+      // se inicia sesión
+      await Data.logIn('testuser@geopromos.co', 'testuser');
+    }
+    // aquí habremos iniciado sesión
+    this.setState({ loading: false });
+  }
+
+  async loadUserFeed() {
+    const feed = await Data.getUserFeedQuery()
+      .find();
+
+    console.log('feed', feed);
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <div>Cargando...</div>;
+    }
+
+    return <div>Funcionó :)</div>;
+  }
+}
+
+render(<App />, document.getElementById('root'));
