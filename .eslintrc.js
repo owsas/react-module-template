76b1290e5ecd74858153cf185e37fc181bbdcf2e
Git Automerge <jcguarinpenaranda@gmail.com>
2017-12-17 15:10:59 -0500

    updated css

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   "extends": "airbnb",
   rules: {
     'class-methods-use-this': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
   },
   env: {
     jest: true,
