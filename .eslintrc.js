d527b68984f78e322583d4ae775450143da34039
Git Automerge <jcguarinpenaranda@gmail.com>
2018-01-19 18:16:01 -0500

    forbidded prop types

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     'class-methods-use-this': 0,
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/no-static-element-interactions': 0,
+    'react/forbid-prop-types': 0
   },
   env: {
     jest: true,
